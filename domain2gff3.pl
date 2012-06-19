6fbe53e88e06e91a86027bfedae7678d303a9a05
Ismail Mitchell <Mitchell.Ismail@gmail.com>
2012-06-19 16:34:58 -0500

    Updates to Part 1

--- domain2gff3.pl
+++ domain2gff3.pl
@@ -10,46 +10,70 @@ use Carp qw( croak );
 use English qw( -no_match_vars );
 use IO::File;
 use Bio::GFF3::LowLevel;
+use Data::Dumper;
 
 our $VERSION = '1.0.0';
 
 #Opening File with IO file handlers to read in a line at a time
 
-my $fh = IO::File->new( 'Dd_trial.txt', 'r' ) or croak "Can't open $ARGV[0] File: $OS_ERROR";
+my $fh = IO::File->new( 'Dd_trial.txt', 'r' )
+or croak "Can't open $ARGV[0] File: $OS_ERROR";
 
 my $running_id;
 my $current_id;
-my @data = ();
-my $i = 0;
+my $data;
+my $input;
 
-while (my $line = $fh->getline) {
-chomp($line);
+while ( my $line = $fh->getline ) {
+    chomp($line);
     my @input_gff = split /\t/, $line;
-      
-  if(defined($running_id))
-  {
-      
-   if(current_id eq running_id)   
-  {    
-      push @data, $line;
-  } 
-  else
-  {
-      write_gff3(@data);
-      undef @data;
-  
-  }
-  
-  }#End of Run/Current IF
-  else{}
     
-}#End of While
-
-
-
-
-
-    sub write_gff3{
+    $current_id = $input_gff[0];
+    
+    if ( $running_id ) {
+        if ( $current_id eq $running_id) {
+            push @$data, $line;
+        }
+        else {
+            write_gff3($data);
+            undef $data;
+        }
+        
+    }    #End of Run/Current IF
+    else { }
     
     
+    
+    $running_id = $current_id;
+    
+}    #End of While
+
+
+
+
+
+sub write_gff3 {
+    
+    
+    my ($data)  = @_;
+    
+    foreach(@$data)
+    {
+        my $parts;
+        my $gff;
+        @$parts = split /\t/, $_;
+        
+        $gff->{seq_id} = $parts->[0];
+        $gff->{source} = $parts->[3];
+        $gff->{type} = $parts->[5];
+        $gff->{start} = $parts->[6];
+        $gff->{end} = $parts->[7];
+        $gff->{score} = $parts->[8];
+        $gff->{strand} = ".";
+        $gff->{phase} = undef;
+        
+        print Dumper(\$gff);
+        
     }
+    
+}
