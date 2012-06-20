1fb2df4d6e461677d9ef2fd38bc6113c5e765066
Ismail Mitchell <Mitchell.Ismail@gmail.com>
2012-06-20 10:41:54 -0500

    Updated Grouping

--- domain2gff3.pl
+++ domain2gff3.pl
@@ -7,9 +7,8 @@
 use warnings;
 use strict;
 use Carp qw( croak );
-use English qw( -no_match_vars );
 use IO::File;
-use Bio::GFF3::LowLevel;
+use Bio::GFF3::LowLevel qw (gff3_format_feature);
 use Data::Dumper;
 
 our $VERSION = '1.0.0';
@@ -17,7 +16,7 @@ our $VERSION = '1.0.0';
 #Opening File with IO file handlers to read in a line at a time
 
 my $fh = IO::File->new( 'Dd_trial.txt', 'r' )
-or croak "Can't open $ARGV[0] File: $OS_ERROR";
+or croak "Can't open $ARGV[0] File: $!";
 
 my $running_id;
 my $current_id;
@@ -31,17 +30,19 @@ while ( my $line = $fh->getline ) {
     $current_id = $input_gff[0];
     
     if ( $running_id ) {
-        if ( $current_id eq $running_id) {
+        if ( $current_id eq $running_id ) {
             push @$data, $line;
         }
         else {
             write_gff3($data);
             undef $data;
+            push @$data, $line;
         }
         
     }    #End of Run/Current IF
-    else { }
-    
+    else {
+        push @$data, $line;
+         }
     
     
     $running_id = $current_id;
@@ -57,22 +58,25 @@ sub write_gff3 {
     
     my ($data)  = @_;
     
-    foreach(@$data)
+    foreach my $line(@$data)
     {
-        my $parts;
+        my @parts;
         my $gff;
-        @$parts = split /\t/, $_;
+        @parts = split /\t/, $line;
         
-        $gff->{seq_id} = $parts->[0];
-        $gff->{source} = $parts->[3];
-        $gff->{type} = $parts->[5];
-        $gff->{start} = $parts->[6];
-        $gff->{end} = $parts->[7];
-        $gff->{score} = $parts->[8];
+        $gff->{seq_id} = $parts[0];
+        $gff->{source} = $parts[3];
+        $gff->{type} = $parts[5];
+        $gff->{start} = $parts[6];
+        $gff->{end} = $parts[7];
+        $gff->{score} = $parts[8];
         $gff->{strand} = ".";
         $gff->{phase} = undef;
+        $gff->{attributes}->{ID} = 0;
+        $gff->{attributes}->{Alias} = $parts[4];
+
+        print gff3_format_feature($gff);
         
-        print Dumper(\$gff);
         
     }
     
