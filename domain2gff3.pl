7b2d662c3009f2191fdd9d7b01aea91530524026
Ismail Mitchell <Mitchell.Ismail@gmail.com>
2012-06-25 15:01:58 -0500

    Object Oriented

--- domain2gff3.pl
+++ domain2gff3.pl
@@ -1,72 +1,89 @@
+package d2gff;
+{
 #!/usr/local/bin/perl
 
 # $Date: 2012-06-13 $
 # $Revision: 1.0.0 $
 # $Source: /usr/local/Desktop/dictyProjects/​domain2gff3.pl $
 
+#Modules
 use warnings;
 use strict;
 use IO::File;
 use Bio::GFF3::LowLevel qw (gff3_format_feature);
 use Data::Dumper;
 use Getopt::Long;
+use Class::Accessor 'antlers';
+
 our $VERSION = '1.0.0';
 
-# setup defaults
-my $input_file  = 'Dd_trial.txt';
-my $output_file = 'output.gff3';
-
-GetOptions(
-    'i|input=s'  => \$input_file,
-    'o|output=s' => \$output_file,
-) or die "Incorrect usage!\n";
-
-#Opening File with IO file handlers to read in a line at a time
-
-my $fh = IO::File->new( $input_file, 'r' )
-  or die "Can't open $input_file File: $!";
-
-my $fh2 = IO::File->new( $output_file, 'w' )
-  or die "Couldn't open file for writing: $!\n";
-
-$fh2->print("##gff-version\t3\n");
-
-my $running_id;
-my $current_id;
-my $data;
-
-while ( my $line = $fh->getline )
-{
-    chomp($line);
-    my @input_gff = split /\t/, $line;
-
-    $current_id = $input_gff[0];
-
-    if ($running_id)
-    {
-        if ( $current_id eq $running_id )
+    has input => (is => 'ro', isa => "Str");
+    has output => (is => 'ro', isa => "Str");
+    
+    
+    
+    sub process_file{
+        
+        
+        my $running_id;
+        my $current_id;
+        my $data;
+        my $self = shift;
+        
+        #Creating Options using Get Long    
+        GetOptions(
+        'i|input=s'  => \$self->input,
+        'o|output=s' => \$self->output,
+        ) or die "Incorrect usage!\n";
+    
+        #Opening File with IO file handlers to read in a line at a time
+        
+        my $fh = IO::File->new( $self->input, 'r' )
+        or die "Can't open $self->input File: $!";
+        
+        my $fh2 = IO::File->new( $self->output, 'w' )
+        or die "Couldn't open file for writing: $!\n";
+        
+        $fh2->print("##gff-version\t3\n");
+        
+        
+        while ( my $line = $fh->getline )
         {
-            push @$data, $line;
-        }
-        else
-        {
-            write_gff3( $data, $fh2 );
-            undef $data;
-            push @$data, $line;
-        }
+            chomp($line);
+            my @input_gff = split /\t/, $line;
+            
+            $current_id = $input_gff[0];
+            
+            if ($running_id)
+            {
+                if ( $current_id eq $running_id )
+                {
+                    push @$data, $line;
+                }
+                else
+                {
+                    write_gff3( $data, $fh2 );
+                    undef $data;
+                    push @$data, $line;
+                }
+                
+            }    #End of Run/Current IF
+            else
+            {
+                push @$data, $line;
+            }
+            
+            $running_id = $current_id;
+            
+        }    #End of While
+    
+        $fh->close;
+        $fh2->close;    
+    
+    } #End of Read File Sub
 
-    }    #End of Run/Current IF
-    else
-    {
-        push @$data, $line;
-    }
 
-    $running_id = $current_id;
 
-}    #End of While
-
-$fh->close;
-$fh2->close;
 
 sub write_gff3
 {
@@ -142,6 +159,14 @@ sub write_gff3
           . "###\n" );
 
 }
+}#End of Package
+
+#Creating New Class
+my $me = d2gff->new({input => 'Dd_trial.txt', output => 'output.txt'});
+
+#Processes the file for the output
+$me->process_file;
+
 
 =head1 NAME
  
@@ -206,7 +231,7 @@ Only works with the InterPro domains of Dictyostelium proteins tab delimited fil
 
 =head1 AUTHOR
  
-I<Isma'il Mitchell> B<[Mitchell.Ismail@gmail.com]> &
+I<Isma'il Mitchell> B<Mitchell.Ismail@gmail.com> &
 I<Siddhartha Basu>  B<siddhartha-basu@northwestern.edu>
  
 =head1 LICENCE AND COPYRIGHT
