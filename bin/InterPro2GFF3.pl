441c0265c537ba88b6f0b4de95e89d0d0a6024d3
Ismail Mitchell <Mitchell.Ismail@gmail.com>
2012-06-26 14:57:54 -0500

    Distribution

---
+++ bin/InterPro2GFF3.pl
@@ -0,0 +1,23 @@
+use strict;
+use InterPro2GFF3;
+use Getopt::Long;
+
+BEGIN { use_ok 'InterPro2GFF3' }
+
+
+my $input = 'Dd_trial.txt';
+my $output = 'Output.gff3';
+
+#Creating Options using Get Long    
+GetOptions(
+'i|input=s'  => \$input,
+'o|output=s' => \$output,
+) or die "Incorrect usage!\n";
+
+
+
+#Creating New Class
+my $me = InterPro2GFF3->new({input => $input , output => $output });
+
+#Processes the file for the output
+$me->process_file;
